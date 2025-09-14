9248bfd3c06fbfd910308835696baf1b80d5f87d
PrinceDev90 <bavishiprince90@gmail.com>
2025-09-14 17:21:46 +0530

    commit from the prince branch

---
+++ src/app/components/princs/princs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-princs',
+  imports: [],
+  templateUrl: './princs.html',
+  styleUrl: './princs.css'
+})
+export class Princs {
+
+}
